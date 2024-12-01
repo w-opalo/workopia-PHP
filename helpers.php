a1374dc8a81cc6acc96ccc8d64bb82d78771f0bb
Willice Opalo <williceopalo11@gmail.com>
2024-12-01 09:51:36 +0300

    job listing

---
+++ helpers.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = "")
+{
+    return __DIR__ . "/" . $path;
+}
+
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    // inspectAndDie($viewPath);
+    // inspect($viewPath);
+
+    if (file_exists($viewPath)) {
+        // extract($data);
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        // extract($data);
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
