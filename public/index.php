5215139eb40744f9f60777b831d1eb9f03716be0
Willice Opalo <williceopalo11@gmail.com>
2024-12-08 11:09:21 +0300

    Using a composer

--- public/index.php
+++ public/index.php
@@ -1,14 +1,17 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require "../helpers.php";
 // require basePath("Framework/Router.php");
 // require basePath('Framework/Database.php');
 
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+use Framework\Router;
+
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 //instanciate the router
 $router = new Router();
